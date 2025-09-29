0508a9c73b510b76d56c019a40da415a7d00c1de
Code-with-Nikhil-07 <144557756+coder-nikhil-07@users.noreply.github.com>
2025-09-30 00:05:27 +0530

    add new Slider and make a responsive

--- src/app/Component/OurProjects/OurProjects.jsx
+++ src/app/Component/OurProjects/OurProjects.jsx
@@ -2,8 +2,7 @@ import React from 'react'
 import style from './OurProjects.module.css'
 import ViewNextSectionButton from '../SectionCommonButton/ViewNextSectionButton'
 import ViewPreviousSectionButton from '../SectionCommonButton/ViewPreviousSectionButton'
-import Square from '../Square/Square'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import ThumbnailSlider from './ThumbnailSlider'
 export default function OurProjects({ ViewNextLayout, ViewPreviousLayout }) {
     return (
         <section className={style.OurProjectsMain}>
@@ -11,20 +10,22 @@ export default function OurProjects({ ViewNextLayout, ViewPreviousLayout }) {
                 <div className='container'>
                     <div className='row'>
                         <div className='col-12 text-center mb-3 mb-md-4 mb-lg-5'>
-                            <div className={`ps-5 position-relative ${style.OurProjectsContent}`}>
+                            <div className={`position-relative ${style.OurProjectTitle}`}>
                                 <h1>Our Projects</h1>
                             </div>
                         </div>
                     </div>
                     <div className='row align-items-center'>
-                        <div className='col-6'>
+                        <div className='col-12 col-md-7'>
                             <div className={`${style.OurProjectThumbnail}`}>
-                                <div className={style.ProjectCard}>
-                                </div>
+                                <ThumbnailSlider />
                             </div>
                         </div>
-                        <div className='col-6'>
-
+                        <div className='col-12 col-md-5'>
+                            <div className={`text-center text-md-start mt-2 mt-md-0 ps-md-2 ps-lg-3 ps-xl-5 position-relative ${style.OurProjectsContent}`}>
+                                <p>Hey! 👋 I’m Nikhil Lodhi – I build modern, responsive websites using skills like HTML, CSS, JavaScript, PHP, React, Next.js, WordPress, and CSS frameworks.</p>
+                                <p>From pure HTML/CSS projects to dynamic React & Next.js apps, and even WordPress custom sites, each project is designed to be interactive, mobile-friendly, and user-focused website.</p>
+                            </div>
                         </div>
                     </div>
                 </div>
